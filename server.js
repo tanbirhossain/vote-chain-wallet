b57e87211500cd6e4b92c0090d9bdbda9dab9436
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2020-06-03 14:14:00 +0900

    update

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./dist/votechainxui'));
-
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/votechainxui/'}),
 );
-
 app.listen(process.env.PORT || 8080);
