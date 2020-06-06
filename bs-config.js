c0a77eb2898096ba2090374b77a96b39bda549ed
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2020-06-06 13:38:50 +0900

    update

--- bs-config.js
+++ bs-config.js
@@ -2,9 +2,8 @@
 var _port= process.env.PORT || 8000;
 module.exports = {
   port: _port,
-  files: ['./**/*.{html,htm,css,js}'],
+  files: ['./dist/votechainxui/**/*.{html,htm,css,js}'],
   server:{
       baseDir: "./dist/votechainxui"
   }
 }
- 
--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.static('./dist/votechainxui'));
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/votechainxui/'}),
+// );
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
+const http = require('http')
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/votechainxui'));
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/votechainxui/'}),
-);
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'dist/votechainxui')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/votechainxui/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
